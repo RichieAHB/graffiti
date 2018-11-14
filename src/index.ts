6098167dcb68da510914a213e1ec6b0c0cd0943c
RichieAHB <richard.beddington@guardian.co.uk>
2018-11-14 21:52:33 +0000

    Add a start button

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ let restart = () => {};
 
 const vars = {
   speed: 1,
-  paused: false,
+  paused: true,
   showPhysics: true,
   planetCount: 3,
   distance: 250,
@@ -49,6 +49,7 @@ const start = () => {
   const system = System.create(
     balancedPlanets(vars.planetCount, vars.distance, vars.minMass, vars.maxMass)
   );
+  renderer.render(system, vars.showPhysics);
   const getDelta = createDeltaGetter();
   let raf = -1;
   const run = () => {
@@ -72,7 +73,7 @@ restart = start();
 const gui = new dat.GUI();
 gui.add(vars, 'speed', -2, 2);
 gui.add(vars, 'scale', 1.01, 3);
-gui.add(vars, 'paused');
+const paused = gui.add(vars, 'paused');
 gui.add(vars, 'showPhysics');
 const folder = gui.addFolder('start conditions (needs restart)');
 folder.add(vars, 'planetCount');
@@ -81,3 +82,15 @@ folder.add(vars, 'minMass');
 folder.add(vars, 'maxMass');
 folder.open();
 gui.add(vars, 'restart');
+
+const startButton = document.getElementById('start');
+
+if (startButton) {
+  startButton.addEventListener('click', () => {
+    paused.setValue(false);
+    const parent = startButton.parentNode;
+    if (parent) {
+      parent.removeChild(startButton);
+    }
+  });
+}
